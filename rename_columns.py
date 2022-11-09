8874a2087d2a1010f177095de742fd009871e312
Dulangi Kanchana <110457839+DulangiK@users.noreply.github.com>
2022-11-09 07:21:39 +0530

    Create rename_columns.py

---
+++ rename_columns.py
@@ -0,0 +1,3 @@
+df = df.rename(columns={'old_col1': 'new_col1', 'old_col2': 'new_col2'})
+
+# here it is important to note that no need to put all column names just te ones needed to rename is sufficient 
